e94ee546a474b0d0f685dd33a61248aa686e1d7f
Cleazyy <cleazy2002@gmail.com>
2023-01-24 10:05:46 +0100

    fix function name

--- srcs/operations/push.c
+++ srcs/operations/push.c
@@ -6,13 +6,13 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:02:47 by fluchten          #+#    #+#             */
-/*   Updated: 2023/01/20 11:43:52 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:05:26 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	ft_push(t_stack **stack1, t_stack **stack2)
+static void	push(t_stack **stack1, t_stack **stack2)
 {
 	t_stack	*temp;
 
@@ -27,12 +27,12 @@ static void	ft_push(t_stack **stack1, t_stack **stack2)
 
 void	pa(t_stack **stack_a, t_stack **stack_b)
 {
-	ft_push(stack_b, stack_a);
+	push(stack_b, stack_a);
 	write(1, "pa\n", 3);
 }
 
 void	pb(t_stack **stack_a, t_stack **stack_b)
 {
-	ft_push(stack_a, stack_b);
+	push(stack_a, stack_b);
 	write(1, "pb\n", 3);
 }
